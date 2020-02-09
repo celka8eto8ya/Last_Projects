a21d5ec20628e4748840f5e56f631f58409dd7da
celka8eto8ya <kostychik@bk.ru>
2020-02-09 14:22:46 +0300

    28.12.2019

--- ElectronicBookkeeping(AK)№2/ElectronicBookkeeping(AK))№2/ElectronicBookkeeping.Designer.cs
+++ ElectronicBookkeeping(AK)№2/ElectronicBookkeeping(AK))№2/ElectronicBookkeeping.Designer.cs
@@ -103,7 +103,7 @@
             this.tabControl1.Multiline = true;
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(690, 443);
+            this.tabControl1.Size = new System.Drawing.Size(689, 446);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -116,7 +116,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(682, 414);
+            this.tabPage1.Size = new System.Drawing.Size(681, 417);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Финансовое состояние";
             // 
@@ -182,7 +182,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(682, 414);
+            this.tabPage2.Size = new System.Drawing.Size(681, 417);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Доходы";
             // 
@@ -326,7 +326,7 @@
             this.tabPage3.Location = new System.Drawing.Point(4, 25);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(682, 414);
+            this.tabPage3.Size = new System.Drawing.Size(681, 417);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Затраты";
             // 
@@ -397,7 +397,7 @@
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox4.Size = new System.Drawing.Size(666, 292);
+            this.textBox4.Size = new System.Drawing.Size(666, 342);
             this.textBox4.TabIndex = 22;
             this.textBox4.Text = resources.GetString("textBox4.Text");
             // 
@@ -676,12 +676,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(690, 443);
+            this.ClientSize = new System.Drawing.Size(689, 446);
             this.Controls.Add(this.tabControl1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "ElectronicBookkeeping";
-            this.ShowInTaskbar = false;
             this.Text = "ElectronicBookkeeping";
             this.Load += new System.EventHandler(this.ElectronicBookkeeping_Load);
             this.tabControl1.ResumeLayout(false);
