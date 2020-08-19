65ed18cc531c5e2eafb3afe9f5c72052d653b892
Andrei Kostiuchik <kostychik@bk.ru>
2020-08-19 21:36:06 +0300

    ###

---
+++ KAM-19-08-2020-E2Book/KAM-19-08-2020-E2Book/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KAM_19_08_2020_E2Book
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Zmeyuka-ProceduralApproach/Form2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp32
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            TopMost = true;
+            InitializeComponent();
+          
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           
+            Bank.Slog = comboBox1.Text;
+             Bank.HH = 1;
+            
+            Bank.Sound = comboBox2.Text;
+            Bank.Wals = comboBox3.Text;
+            this.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
