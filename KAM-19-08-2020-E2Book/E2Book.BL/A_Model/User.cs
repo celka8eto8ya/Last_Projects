b8b83520d8c1ca9ff8c265e8e55c942d13606639
Andrei Kostiuchik <kostychik@bk.ru>
2020-09-29 12:12:46 +0300

    Model.User and Controller.UserController with Serialize

---
+++ KAM-19-08-2020-E2Book/E2Book.BL/A_Model/User.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+
+namespace E2Book.BL.A_Model
+{
+    [Serializable]
+    public class User
+    {
+        // Password for enter in App
+        private string password;
+        // Total amount money on Account
+        private double totalSum;
+        // Total amount of Accounts
+        private readonly byte amountOfAccount;
+        // Name of Server for create DB
+        private string nameOfServer;
+        // User's name of Server for create DB
+        private string nameOfUser;
+        // password of Server for create DB
+        private string passwordOfServer;
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    password = value;
+                }
+            }
+        }
+        public double TotalSum
+        {
+            get
+            {
+                return totalSum;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    totalSum = value;
+                }
+            }
+        }
+        public byte AmountOfAccount
+        {
+            get
+            {
+                return amountOfAccount;
+            }
+            set
+            {
+                if (value >= 0 && value < 250)
+                {
+                    totalSum = value;
+                }
+            }
+        }
+        public string NameOfServer
+        {
+            get
+            {
+                return nameOfServer;
+            }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    nameOfServer = value;
+                }
+            }
+        }
+        public string NameOfUser
+        {
+            get
+            {
+                return nameOfUser;
+            }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    nameOfUser = value;
+                }
+            }
+        }
+        public string PasswordOfServer
+        {
+            get
+            {
+                return passwordOfServer;
+            }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    passwordOfServer = value;
+                }
+            }
+        }
+
+
+        public User(string password1)
+        {
+            Password = password1;
+            TotalSum = 0;
+            AmountOfAccount = 0;
+
+        }
+
+        public User(string password1, string nameOfServer1, string nameOfUser1, string passwordOfServer1)
+        {
+            Password = password1;
+            TotalSum = 0;
+            AmountOfAccount = 0;
+
+            NameOfServer = nameOfServer1;
+            NameOfUser = nameOfUser1;
+            PasswordOfServer = passwordOfServer1;
+        }
+
+
+
+    }
+}
---
+++ KAM-19-08-2020-E2Book/E2Book.BL/C_Controller/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using E2Book.BL.A_Model;
+
+
+
+namespace E2Book.BL.C_Controller
+{
+    public class UserController
+    {
+        public static void SaveInfoUser(TextBox sourceData, TextBox password, TextBox serverName, TextBox userName, TextBox serverPassword)
+        {
+            if (sourceData.Text == ".txt")
+            {
+                User user1 = new User(password.Text);
+
+                List<User> usersTxt = new List<User>() { user1 };
+
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                using (FileStream fs = new FileStream("dataTxt.dat", FileMode.OpenOrCreate))
+                {
+
+                }
+            }
+            else
+            {
+                User user1 = new User(password.Text, serverName.Text, userName.Text, serverPassword.Text);
+
+                List<User> usersDB = new List<User>() { user1 };
+
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                using (FileStream fs = new FileStream("dataTxt.dat", FileMode.OpenOrCreate))
+                {
+                    formatter.Serialize(fs,usersDB);
+                }
+            }
+
+        }
+    }
+}
